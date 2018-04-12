f9b3d52c9ca9fb29e77087f24b1d0dd3f989eab0
kaizu <kwaizu@gmail.com>
2018-04-12 20:37:04 +0900

    Fixed

--- bioimaging/image.py
+++ bioimaging/image.py
@@ -5,10 +5,10 @@ import numpy
 def convert_8bit(data, cmin=None, cmax=None, low=None, high=None):
     """Same as scipy.misc.bytescale"""
     data = numpy.asarray(data)
-    cmin = cmin or data.min()
-    cmax = cmax or data.max()
-    low = low or 0
-    high = high or 255
+    cmin = data.min() if cmin is None else cmin
+    cmax = data.max() if cmax is None else cmax
+    low = 0 if low is None else low
+    high = 255 if high is None else high
 
     cscale = cmax - cmin
     scale = float(high - low) / cscale
@@ -18,8 +18,8 @@ def convert_8bit(data, cmin=None, cmax=None, low=None, high=None):
 
 def save_as_image(filename, data, cmap=None, low=None, high=None):
     data = numpy.asarray(data)
-    low = low or data.min()
-    high = high or data.max()
+    low = data.min() if low is None else low
+    high = data.max() if high is None else high
 
     import matplotlib
     matplotlib.use('Agg')
@@ -29,6 +29,9 @@ def save_as_image(filename, data, cmap=None, low=None, high=None):
     plt.imsave(filename, data, cmap=cmap, vmin=low, vmax=high)
 
 def convert_npy_to_8bit_image(filename, output=None, cmap=None, cmin=None, cmax=None, low=None, high=None):
+    low = 0 if low is None else low
+    high = 255 if high is None else high
+
     output = output or '{}.png'.format(os.path.splitext(filename)[0])
 
     data = numpy.load(filename)
