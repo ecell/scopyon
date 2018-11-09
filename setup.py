75e93c52b00b0b123dbfe6615e7988456e036e25
kaizu <kwaizu@gmail.com>
2018-11-09 18:36:48 +0900

    Check CIRCLE_TAG if exists

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+from setuptools.command.install import install
+import os
 import os.path
+import sys
 import glob
 
 
@@ -15,12 +18,25 @@ version = next(
      if line.startswith('__version__ = ')),
     '0.0.dev0')  ## default
 
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+class verify_version_command(install):
+    description = "verify that the git tag matches the version"
+
+    def run(self):
+        tag = os.getenv('CIRCLE_TAG')
+        if tag is not None and tag != version:
+            info = "Git tag: {0} does not match the version of this library: {1}".format(tag, version)
+            sys.exit(info)
+
 setup(
     name='scopyon',
     version=version,
     url='https://github.com/ecell/scopyon',
     description='Monte Carlo simulation toolkit for bioimaging systems',
-    long_description=open('README.md').read(),
+    long_description=readme(),
     long_description_content_type='text/markdown',
     author='Masaki Watabe',
     author_email='masaki@riken.jp',
@@ -34,10 +50,15 @@ setup(
     test_suite='test',
     license='BSD-3-Clause',
     classifiers=[
+        'Development Status :: 3 - Alpha',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
         # 'Topic :: Scientific/Engineering :: Physics',
         # 'Topic :: Scientific/Engineering :: Visualization',
         'License :: OSI Approved :: BSD License',
         ],
+    python_requires='>=3',
+    cmdclass={'verify': verify_version_command}
     )
