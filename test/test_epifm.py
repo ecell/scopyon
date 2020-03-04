81b9a2e8d549a0292418722301438c6f7bd99f85
kaizu <kwaizu@gmail.com>
2020-03-05 01:06:10 +0900

    Add a new test

---
+++ test/test_epifm.py
@@ -0,0 +1,17 @@
+import unittest
+
+
+class TestEPIFM(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test1(self):
+        import scopyon._epifm
+
+
+if __name__ == '__main__':
+    unittest.main()
