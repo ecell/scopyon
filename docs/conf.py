68d7b143477cf6cde8b01a6976c94a5ade9e796f
kaizu <kwaizu@gmail.com>
2020-06-04 17:44:55 +0900

    Collapse navigation

--- docs/conf.py
+++ docs/conf.py
@@ -113,7 +113,7 @@ html_theme = 'sphinx_rtd_theme'
 # documentation.
 #
 html_theme_options = {
-    "collapse_navigation" : False
+    "collapse_navigation" : True,
     }
 
 # Add any paths that contain custom static files (such as style sheets) here,
